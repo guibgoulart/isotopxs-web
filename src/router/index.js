73fa2aa94c05d2a307c2f9286e2b1992a8ebde06
guibg <gui.bgoulart@gmail.com>
2024-06-08 01:55:35 -0300

    feat: add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    // Add your routes here
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    }
   ]
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="background h-screen w-screen">
+    <!-- Foreground Content -->
+    <div class="flex flex-col items-center justify-center h-full text-center relative">
+      <!-- 404 Text on top of the gif -->
+      <h1 class="absolute top-1/4 w-full text-6xl text-white font-papyrus">404</h1>
+      <img src="../assets/images/spieker.gif" alt="Spieker Bebendo" class="w-1/2 mx-auto" />
+      <h2 class="text-2xl mt-8 text-white">Ta perdido paizinho?</h2>
+      <!-- Link to return to the home page -->
+      <a href="/" class="mt-4 text-white">Volta pra home</a>
+    </div>
+  </div>
+</template>
+
+<style>
+.background {
+  background-image: url('../assets/images/palhaco.jpg');
+  background-size: cover;
+  background-repeat: repeat;
+}
+</style>
